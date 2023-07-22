461eeb0d4e661e7fa53d2c2e99bd97345f1fc344
Sergio <sergiomolinaflores1@gmail.com>
2023-07-22 08:48:14 -0400

    Permiso Internet Ok, viewBinding Ok y dependencias Navigation Ok

---
+++ app/src/main/java/cl/samf/sprint_final_mod5/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.samf.sprint_final_mod5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
